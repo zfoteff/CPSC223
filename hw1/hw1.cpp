8e9a12121b61364dd7f824cb25fb6498cdf363fc
zfoteff <zfoteff@zagmail.gonzaga.edu>
2019-08-27 11:58:42 -0700

    Initialize files for hw1

---
+++ hw1/hw1.cpp
@@ -0,0 +1,20 @@
+/*
+* Class: CPSC 223-01
+* Zac Foteff
+* GU Username: zfoteff
+* File Name: hw1.cpp
+    This program...
+* To build:   g++ hw1.cpp
+* To execute: ./a.out
+*/
+
+#include "hw1.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+}
+
+return 0;
---
+++ hw1/hw1.h
@@ -0,0 +1,9 @@
+/*
+* Class: CPSC 223-01
+* Zac Foteff
+* GU Username: zfoteff
+* File Name: hw1.h
+    Header file for hw1.cpp.
+* To build:   g++ hw1.cpp
+* To execute: ./a.out
+*/
